5b7219e832bbc5ed74c90b958551f0a7ea5a8dc7
Luc Wicki <Luc_Wicki@sluz.ch>
2023-03-01 16:34:19 +0100

    Husi done, nach mim Verstöändnis

---
+++ laravel/resources/views/Anmeldung.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Anmeldung</title>
+</head>
+<body>
+<form>
+    <input type="email" placeholder="Email"><br>
+    <input type="text" placeholder="Name"><br>
+    <input type="text" placeholder="Vorname"><br>
+    <input type="textarea" placeholder="Bemerkungen"><br>
+    <input type="text" placeholder="IP Adresse"><br>
+    <input type="datetime-local" placeholder="Zeit der Anmdeldung"><br>
+    <input type="text" placeholder="Session ID"><br>
+
+</form>
+</body>
+</html>
